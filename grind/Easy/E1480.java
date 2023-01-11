25c572e0d04cf271f2d495b1816290bc0d4d3d11
jchou21 <jared.chou7@gmail.com>
2023-01-10 21:10:47 -0500

    initial commit from day 1

---
+++ grind/Easy/E1480.java
@@ -0,0 +1,35 @@
+package grind;
+
+// Running Sum of 1d Array
+class Solution {
+    private class ArrayStack {
+        int length;
+        int index;
+        int[] stack;
+
+        public ArrayStack(int length){
+            this.length = length;
+            this.index = 0;
+            this.stack = new int[length];
+        }
+
+        public boolean push(int val){
+            if (index >= length) {
+                return false;
+            }else{
+                stack[index++] = val;
+                return true;
+            }
+        }
+    }
+
+    public int[] runningSum(int[] nums) {
+        ArrayStack runSum = new ArrayStack(nums.length);
+        int prev = 0;
+        for (int i = 0; i < nums.length; i++){
+            prev += nums[i];
+            runSum.push(prev);
+        }
+        return runSum.stack;
+    }
+}
---
+++ grind/Easy/E724.java
@@ -0,0 +1,29 @@
+package grind;
+
+class E724 {
+    public int pivotIndex(int[] nums) {
+        int leftSum = 0;
+        int rightSum = 0;
+        for(int i = 1; i < nums.length; i++){
+            rightSum += nums[i];
+        }
+        if(leftSum == rightSum){
+            return 0;
+        } 
+        else {
+            for(int i = 1; i < nums.length-1; i++){
+                leftSum += nums[i-1];
+                rightSum -= nums[i];
+                if(leftSum == rightSum) return i;
+            }
+            if(nums.length >= 2){
+                leftSum += nums[nums.length - 2];
+                rightSum -= nums[nums.length - 1];
+                if(leftSum == rightSum){
+                    return nums.length - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
