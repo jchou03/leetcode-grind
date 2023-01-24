787ab7d6f17a2752b78560d70db1fe19080e91d3
jchou21 <jared.chou7@gmail.com>
2023-01-23 22:35:43 -0500

    easy DP problem

---
+++ grind/Easy/E35.java
@@ -0,0 +1,22 @@
+package grind.Easy;
+
+// search insert position
+
+public class E35 {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0; 
+        int high = nums.length - 1;
+        if(nums[0] > target) return 0;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                low = mid + 1;
+            }else if(nums[mid] > target){
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+}
