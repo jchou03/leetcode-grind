37612099c02b1263fdc563dca727c3c42855e462
jchou21 <jared.chou7@gmail.com>
2023-02-22 21:24:00 -0500

    E338 improved runtime dramatically, need to improve memory efficiency,

--- grind/Easy/E338.java
+++ grind/Easy/E338.java
@@ -1,14 +1,13 @@
 package grind.Easy;
 
 // counting bits
-// runtime: 59%, memory: 5%
+// runtime: 99%, memory: 5%
 
 public class E338 {
     public int[] countBits(int n) {
         int[] oneBits = new int[n+1];
         oneBits[0] = 0;
-        if(n >= 1) oneBits[1] = 1;
-        for(int i = 2; i <= n; i++){
+        for(int i = 1; i <= n; i++){
             if(i % 2 == 1){
                 oneBits[i] = oneBits[i/2] + 1;
             }else{
