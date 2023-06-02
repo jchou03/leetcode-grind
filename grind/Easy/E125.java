c0117f2baf83b547aeb047ea7b678b49ed4285a8
jchou21 <jared.chou7@gmail.com>
2023-06-01 21:59:01 -0400

    working on E125 (need to leave for now)

---
+++ grind/Easy/E125.java
@@ -0,0 +1,29 @@
+package grind.Easy;
+
+// 2 pointer palendrome solution (in progress)
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int p1 = 0;
+        int p2 = s.length() -1;
+        while(p1 < p2){
+            while(p1 < s.length() && !Character.isAlphanumeric(s.charAt(p1))){
+                p1++;
+            }
+            while( p2 >= 0 && !Character.isAlphanumeric(s.charAt(p2))){
+                p2--;
+            }
+            // System.out.println("p1: " + s.charAt(p1) + " p2: " + s.charAt(p2));
+            if(p1 >= s.length() || p2 < 0){
+                return true;
+            }else if(s.charAt(p1) != s.charAt(p2)){
+                return false;
+            }else{
+                p1++;
+                p2--;
+            }
+        }
+        return true;
+    }
+}
