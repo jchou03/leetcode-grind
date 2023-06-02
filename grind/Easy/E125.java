9fe283b6cfb936d405f9fd48d225454de95f53bd
jchou21 <jared.chou7@gmail.com>
2023-06-02 18:39:07 -0400

    currently attempting an O(1) solution for array rotation

--- grind/Easy/E125.java
+++ grind/Easy/E125.java
@@ -28,3 +28,50 @@ class Solution {
         return true;
     }
 }
+
+// attempted solution for O(1) space
+/*
+ * class Solution {
+    public void rotate(int[] nums, int k) {
+        // use algo to jump around the array starting at 0, then at 0-k, then 0-2k, etc
+        // store the number we just replaced for moving it to the next step
+        // use an index counter to stop when the array is all done
+        int count = 0;
+        int i = 0;
+        int prev = nums[i];
+        int temp = prev;
+        k = k % nums.length;
+        if(nums.length == 1 || k == 0) return;
+        if(nums.length % k == 0){
+            // complete each of the smaller loops
+            for(int j = 0; j < nums.length / (nums.length / k); j++){
+                i = j;
+                prev = nums[j];
+                do{
+                    i = (i + k < nums.length) ? (i + k) : (i + k) % nums.length;
+                    temp = nums[i];
+                    nums[i] = prev;
+                    prev = temp;
+                    System.out.println("i is: " + i + " prev: " + prev + " temp: " + temp + Arrays.toString(nums));
+                } while(i != j);
+            }
+        }else{
+            while(count < nums.length){
+                i = (i + k < nums.length) ? (i + k) : (i + k) % nums.length;
+                temp = nums[i];
+                nums[i] = prev;
+                prev = temp;
+                count++;
+                if(i == 0 && count < nums.length){
+                    i++;
+                    count--;
+                    prev = nums[i];
+                }             
+                System.out.println("i is: " + i + " prev: " + prev + " temp: " + temp + Arrays.toString(nums));
+            }
+        }
+
+    }
+}
+ * 
+ */
