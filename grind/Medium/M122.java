93cc9b675fea21f85278f4c84856dd0be808f82c
jchou21 <jared.chou7@gmail.com>
2023-06-03 02:19:54 -0400

    improved memory usage to 33%

--- grind/Medium/M122.java
+++ grind/Medium/M122.java
@@ -1,27 +1,27 @@
 package grind.Medium;
 
 // best time to buy/sell stock 2
-// runtime: 97%, memory: 11%
+// runtime: 97%, memory: 33%
 
 class Solution {
     public int maxProfit(int[] prices) {
-        int curMin = Integer.MAX_VALUE;
-        int curMax = Integer.MIN_VALUE;
+        int curMin = -1;
+        int curMax = -1;
         int profit = 0;
 
         for(int i = 0; i < prices.length; i++){
             if(prices[i] < curMax){
                 profit += (curMax - curMin > 0) ? (curMax - curMin) : 0;
                 curMin = prices[i];
-                curMax = Integer.MIN_VALUE;
-            }else if(curMin > prices[i]){
+                curMax = -1;
+            }else if(curMin == -1 || curMin > prices[i]){
                 curMin = prices[i];
             }else if(curMax < prices[i]){
                 curMax = prices[i];
             }
             // System.out.println("curMin: " + curMin + " curMax: " + curMax + " profit: " + profit);
         }
-        if(curMax != Integer.MIN_VALUE && curMin != Integer.MAX_VALUE && curMax - curMin > 0) profit += (curMax - curMin);
+        if(curMax != -1 && curMin != -1 && curMax - curMin > 0) profit += (curMax - curMin);
         return profit;
     }
 }
