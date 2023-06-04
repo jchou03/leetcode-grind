92035c68b8e9b9f4df46a25aa851bc448a055181
jchou21 <jared.chou7@gmail.com>
2023-06-04 17:44:33 -0400

    initial test cases passed, but need to address case of [1,1,1,1]

--- grind/need_improvement/M45.java
+++ grind/need_improvement/M45.java
@@ -6,18 +6,22 @@ class Solution {
     public int jump(int[] nums) {
         // simple solution: iterate from the back of the array and store the number of jumps needed to reach the end from each index
         // at each index check for the lowest number in range then return the number in the 0th index
-        if(nums.length <= 1) return 0;
+        nums[nums.length - 1] = 0;
 
         for(int i = nums.length - 2; i >= 0; i--){
-            if(i + nums[i] >= nums.length){
+            if(i + nums[i] >= nums.length - 1){
                 nums[i] = 1;
-            }
-            int min = Integer.MAX_VALUE;
-            for(int j = 0; i + j < (Math.min(i + nums[i], nums.length)); j++){
-                if(nums[i + j] < min){
-                    min = nums[i + j];
+            }else{
+                int min = Integer.MAX_VALUE;
+                for(int j = 0; i + j < (Math.min(i + nums[i], nums.length)); j++){
+                    if(nums[i + j] < min){
+                        min = nums[i + j];
+                    }
+                    nums[i] = min + 1;
                 }
             }
         }
+        System.out.println(Arrays.toString(nums));
+        return nums[0];
     }
 }
