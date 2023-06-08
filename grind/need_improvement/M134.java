be4d97c2dbcedc7d39c34c78c6de2ba83c43a550
jchou21 <jared.chou7@gmail.com>
2023-06-07 20:36:15 -0400

    updated with O(n) one pass solution

--- grind/need_improvement/M134.java
+++ grind/need_improvement/M134.java
@@ -1,30 +1,29 @@
 package grind.need_improvement;
 
 // check if it is possible to make the trip along circle road going from gas station to gas station
-// implemented trivial solution
+// O(n) single pass solution
+// runtime: 35%, memory: 76%
 
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         // trivial solution (go around the entire ring and check each gas station)
         int gTank = 0;
+        int posGas = 0;
+        int posIndex = 0;
         for(int i = 0; i < gas.length; i++){
             gTank += (gas[i] - cost[i]);
+            posGas += (gas[i] - cost[i]);
+            if(posGas < 0){
+                posGas = 0;
+                posIndex = i + 1;
+            }
         }
 
         if(gTank < 0){
             System.out.println("no solution");
             return -1;
         }else{
-            // check for which index 
-            for(int i = 0; i < gas.length; i++){
-                gTank = 0;
-                for(int j = 0; j < gas.length; j++){
-                    gTank += (gas[(i + j) % gas.length] - cost[(i + j) % gas.length]);
-                    if(gTank < 0) break;
-                }
-                if(gTank >= 0) return i;
-            }
+            return posIndex;
         }
-        return -1;
     }
 }
