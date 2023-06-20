4e94d5ba652a311628de4a43654543f99fb658ef
jchou21 <jared.chou7@gmail.com>
2023-06-19 23:19:10 -0400

    rotate linked list by using multiple pass and 2 pointer solution and rotating left

--- CodePath Advanced TIP/Week 2/RotateLinkedList.java
+++ CodePath Advanced TIP/Week 2/RotateLinkedList.java
@@ -7,20 +7,39 @@ public class RotateLinkedList {
        ListNode(int x) { val = x; }
     }
 
-    public ListNode rotateLinkedList(ListNode head, int i){
+    public ListNode rotateRight(ListNode head, int i){
+
+//  first get length of LL
+// then rotate LL to the left by length - i
+
+        int len = LLlength(head);
+        if(len == 0) return head;
+        // System.out.println("len: " + len);
         ListNode prev = new ListNode(-1);
         prev.next = head;
         ListNode h = head;
-        for(int j = 0; j < i; j++){
+        for(int j = 0; j < (len - (i%len)); j++){
+            // System.out.println(j);
             prev = h;
-            if(h.next != null){
-                h = h.next;
-            }else{
-                h = head;
-            }
+            h = h.next;
         }
-
         
+
+        prev.next = null;
+        return h;
+    }
+
+    public int LLlength(ListNode head){
+
+        if(head == null) return 0;
+        ListNode prev = head;
+        int i = 1;
+        while(prev.next != null){
+            i++;
+            prev = prev.next;
+        }
+        prev.next = head;
+        return i;
     }
 
 }
