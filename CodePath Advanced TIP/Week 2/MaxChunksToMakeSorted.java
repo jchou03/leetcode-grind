994394360f55a8d4f75d078c25e54e3eb00a170c
jchou21 <jared.chou7@gmail.com>
2023-06-23 23:51:28 -0400

    working on implementing more optimal solution using 2 pass technique

--- CodePath Advanced TIP/Week 2/MaxChunksToMakeSorted.java
+++ CodePath Advanced TIP/Week 2/MaxChunksToMakeSorted.java
@@ -6,12 +6,20 @@ Understand:
 
 Match: 
     - array problem
+    - Hash & Store
+    - 2 pointer
+    - Sort
 
 Plan:
     - use a recursive approach where you try to split it once (then recursivly call it on the smaller bits)
     - on individual method call
         - iterate over the range in array (check if this is a valid splitting index)
         - trivial solution O(n^2)
+
+    better solution:
+    - use multiple passes
+    - use an array to store the minimum value on the right side of an index (inclusive)
+    - then iterate over the original array, and if the max value on the left is less or equal to the min on the right, then increment number of chunks
 
 Examine:
     - inefficient solution
@@ -23,28 +31,24 @@ Examine:
 
 public class MaxChunksToMakeSorted {
     public int maxChunksToSorted(int[] arr) {
-        return 1 + helper(arr, 0, arr.length);
-    }
-    // total array, start index (0 indexed), end index (not inclusive)
-    private int helper(int[] arr, int start, int end){
-        int frontMax;
-        int backMin;
-        for(int i = start; i < end; i++){
-            frontMax = -1;
-            backMin = -1;
-            for(int j = start; j < end; j++){
-                if(j <= i && (frontMax == -1 || frontMax < arr[j])){
-                    frontMax = arr[j];
-                }
-                if(j > i && (backMin == -1 || backMin > arr[j])){
-                    backMin = arr[j];
-                }
+        int numChunks = 0;
+        int leftMax = arr[0];
+        int[] rightMins = new int[arr.length];
+        int tempMin = arr[arr.length - 1];
+        for(int i = arr.length - 1; i >= 0; i--){
+            if(arr[i] < tempMin){
+                tempMin = arr[i];
             }
-            if(frontMax != -1 && backMin != -1 && frontMax <= backMin){
-                System.out.printf("frontMax: %d backMin: %d and i: %d\n", frontMax, backMin, i);
-                return 1 + helper(arr, start, i + 1) + helper(arr, i + 1, end);
+            rightMins[i] = tempMin;
+        }
+        for(int i = 0; i < arr.length; i++){
+            if(leftMax < arr[i]){
+                leftMax = arr[i];
+            }
+            if(leftMax <= rightMins[i]){
+                numChunks++;
             }
         }
-        return 0;
+        return numChunks;
     }
 }
