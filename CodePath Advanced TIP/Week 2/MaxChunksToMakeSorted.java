db89a9de7b1261959a4e5583cc0cdbc1e63d94cb
jchou21 <jared.chou7@gmail.com>
2023-06-26 15:08:04 -0400

    updated solution to H768 to run in O(n) instead of O(n^3)

--- CodePath Advanced TIP/Week 2/MaxChunksToMakeSorted.java
+++ CodePath Advanced TIP/Week 2/MaxChunksToMakeSorted.java
@@ -4,6 +4,18 @@ Understand:
     - given array
     - determine what the maximum number of chunks (that when sorted) 
 
+    happy cases: 
+        - [5, 2, 1, 5, 2] -> 1
+        - [2,1,3,4,4] -> 4
+        - [4,2,1,4,10,3] -> 1 (the last element is smaller than the first element, therefore it has to be sorted in a single one)
+        - [1,0,1,3,2] -> 3
+        - [0,0,1,1,1] -> 5
+        - [1,2,0,3] -> 2
+
+    edge case:
+        - [1] -> 1
+        - [] -> 1
+
 Match: 
     - array problem
     - Hash & Store
@@ -18,26 +30,51 @@ Plan:
 
     better solution:
     - use multiple passes
-    - use an array to store the minimum value on the right side of an index (inclusive)
+    - use an array to store the minimum value on the right side of an index (exclusive)
     - then iterate over the original array, and if the max value on the left is less or equal to the min on the right, then increment number of chunks
 
 Examine:
     - inefficient solution
     - runtime: O(n^3)
     - memory: O(1)
+
+    updated solution:
+        - runtime: O(n)
+        - memory: O(n)
  */
 
 //  runtime: 5% memory: 5%
 
 public class MaxChunksToMakeSorted {
-    public int maxChunksToSorted(int[] arr) {
-        int numChunks = 0;
+
+    public static void main(String[] args){
+        int[][] tests = new int[][]{new int[]{5, 2, 1, 5, 2},
+                                    new int[]{2,1,3,4,4},
+                                    new int[]{4,2,1,4,10,3},
+                                    new int[]{1,0,1,3,2},
+                                    new int[]{0,0,1,1,1},
+                                    new int[]{1,2,0,3} };
+        int[] answers = new int[]{1, 4, 1, 3, 5, 2};
+        for(int i = 0; i < tests.length; i++){
+            System.out.printf("Attempting test: %d\n", i + 1);
+            int res = maxChunksToSorted(tests[i]);
+            System.out.printf("Test %s, Result: %d, Answer: %d\n\n", (res == answers[i] ? "Passed" : "Failed"), res, answers[i]);
+        }
+    }
+
+    public static int maxChunksToSorted(int[] arr) {
+        // handle edge case
+        if(arr.length <= 1) return 1;
+
+        int numChunks = 1;
         int leftMax = arr[0];
         int[] rightMins = new int[arr.length];
-        int tempMin = arr[arr.length - 1];
-        for(int i = arr.length - 1; i >= 0; i--){
-            if(arr[i] < tempMin){
-                tempMin = arr[i];
+        int tempMin = arr[arr.length-1];
+
+        // populate the rightmin array
+        for(int i = arr.length - 2; i >= 0; i--){
+            if(arr[i + 1] < tempMin){
+                tempMin = arr[i + 1];
             }
             rightMins[i] = tempMin;
         }
@@ -49,6 +86,6 @@ public class MaxChunksToMakeSorted {
                 numChunks++;
             }
         }
-        return numChunks;
+        return numChunks > arr.length ? arr.length : numChunks;
     }
 }
