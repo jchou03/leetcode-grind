b4045c3a88c01622e1414fd6fae6eb766e60b04e
jchou21 <jared.chou7@gmail.com>
2023-06-29 21:42:39 -0400

    find the celebrity solution

--- CodePath Advanced TIP/Week 3/FindACelebrity.java
+++ CodePath Advanced TIP/Week 3/FindACelebrity.java
@@ -18,8 +18,44 @@ Plan:
 import java.util.*;
 
 public class FindACelebrity {
-    public int findCelebrity(int[] people){
-        ArrayList<Integer> potentialCelebrities = new ArrayList<Integer>();
-        
+    public int findCelebrity(int n){
+        // approach this problem by trying to reduce the number of celebrity candidates
+        // use a stack to keep track of the potential candidates
+        // while the stack has potential candidates, take the top two and ask if a knows b
+            // if a knows b, then a isn't a celebrity but b might be (push back onto the stack)
+            // if a doesn't know b, then b isn't a celebrity but a might be (push back onto the stack)
+        Stack<Integer> stack = new Stack<Integer>();
+        for(int i = 0; i < n; i++){
+            stack.push(i);
+        }
+
+        int a, b;
+        // when the stack is bigger than 1, there are more than 1 candidates
+        while(stack.size() > 1){
+            a = stack.pop();
+            b = stack.pop();
+            if(knows(a, b)){
+                // a knows b, so a isn't a celebrity but b is still a candidate
+                stack.push(b);
+            }else{
+                // a doesn't know b, so b isn't a celebrity but a might be
+                stack.push(a);
+            }
+        }
+        // now the stack has the last celebrity candidate
+        // verify they are a celebrity by checking whether or not they don't know everyone (and that everyone else knows them)
+        int c = stack.pop();
+        for(int i = 0; i < n; i++){
+            if(!knows(i, c) || knows(c, i)){
+                return -1;
+            }
+        }
+
+        return 1;
+
+    }
+    // function to let code compile, checks if a knows b by checking the underlying graph
+    public static boolean knows(int a, int b){
+        return false;
     }
 }
