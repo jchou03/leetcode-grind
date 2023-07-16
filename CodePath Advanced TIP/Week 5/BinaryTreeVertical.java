bdc7480b64ba1ce64a094ea11a423162455e94b6
jchou21 <jared.chou7@gmail.com>
2023-07-15 20:22:49 -0400

    implemented binarytreevertical using a sorted map (treemap) and and hashing

--- CodePath Advanced TIP/Week 5/BinaryTreeVertical.java
+++ CodePath Advanced TIP/Week 5/BinaryTreeVertical.java
@@ -5,14 +5,52 @@ undersatnd
 
 Match
     in order traversal (recursion)
+    use a hash map to store each node to a coordinate relative to the root
 
 Plan
-    use a stack to do dfs in order traversal
-    - while cur node has left child, push cur onto stack to visit later then make cur cur.left
-    - keep going to the left until we find the leftmost value (this is 0)
-    -
+    use a recursive in order solution in order populate a hashmap storing the nodes in each column relative to 0 (root)
+    then return list
 
  */
+import java.util.*;
 public class BinaryTreeVertical {
-    
+    public class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+    }
+    public static void main(String[] args){
+        BinaryTreeVertical c = new BinaryTreeVertical();
+        TreeNode root = c.new TreeNode(1, c.new TreeNode(3, c.new TreeNode(5), c.new TreeNode(3)), c.new TreeNode(2, null, c.new TreeNode(9)));
+        
+    }
+
+    TreeMap<Integer, List<Integer>> map = new TreeMap<Integer, List<Integer>>();
+    public List<List<Integer>> binaryTreeVertical (TreeNode root){
+        helper(root, 0);
+        List<List<Integer>> res = new LinkedList<List<Integer>>();
+        for(Map.Entry<Integer, List<Integer>> i : map.entrySet()){
+            res.add(i.getValue());
+        }
+        return res;
+    }
+    private void helper(TreeNode root, int col){
+        if(root == null) return;
+        if(map.containsKey(col)){
+            map.get(col).add(root.val);
+        }else{
+            LinkedList<Integer> list = new LinkedList<Integer>();
+            list.add(root.val);
+            map.put(col, list);
+        }
+        helper(root.left, col-1);
+        helper(root.right, col + 1);
+    }
 }
