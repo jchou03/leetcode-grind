7f7cb317c1656e0aa9564a9ad0795f832c7405d9
jchou21 <jared.chou7@gmail.com>
2023-07-30 13:21:34 -0400

    Updated M11 Container with most water with 2 pointer solution

--- grind/Medium/M11.java
+++ grind/Medium/M11.java
@@ -1,16 +1,23 @@
 package grind.Medium;
 
 // container with the most water
-// currently wrote O(n^2) solution, takes too long
+/*
+ * runtime: O(n) 22%
+ * memory: O(1) 72%
+ */
 
 public class M11 {
     public int maxArea(int[] height) {
-        // simple solution, write O(n^2) algo to check every combination of lines
         int maxWater = 0;
-        for(int i = 0; i < height.length; i++){
-            for(int j = 0; j < height.length; j++){
-                int water = (j - i) * Math.min(height[i], height[j]);
-                if(water > maxWater) maxWater = water;
+        int p1 = 0;
+        int p2 = height.length-1;
+        while(p1 < p2){
+            int curWater = Math.min(height[p1], height[p2]) * (p2 - p1);
+            maxWater = Math.max(maxWater, curWater);
+            if(height[p1] < height[p2]){
+                p1++;
+            }else{
+                p2--;
             }
         }
         return maxWater;
