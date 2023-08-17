e28683c24bb425527c5af23b6671afda7ac72723
jchou21 <jared.chou7@gmail.com>
2023-08-16 20:52:47 -0400

    updated stack solution

--- grind/Medium/M71.java
+++ grind/Medium/M71.java
@@ -10,37 +10,63 @@ public class M71 {
     public String simplifyPath(String path) {
         Stack<String> stack = new Stack<String>();
         String[] strs = path.split("/");
+        
         for(int i = 0; i < strs.length; i++){
-            stack.push(strs[i]);
-        }
-
-        /*
-        split the strings by '/' and put them onto the stack
-        from the stack, add into the resulting string based on the 
-        */
-        // System.out.println(Arrays.toString(strs));
-
-        String res = "";
-        int skips = 0;
-        while(!stack.isEmpty()){
-            String cur = stack.pop();
+            String cur = strs[i];
             switch(cur){
+                case "":
+                    break;
                 case "..":
-                    skips++;
+                    if(!stack.isEmpty()){
+                        stack.pop();
+                    }
                     break;
                 case ".":
                     break;
-                case "":
-                    break;
                 default:
-                    if(skips == 0){
-                        res = "/" + cur + res;
-                    }else{
-                        skips--;
-                    }
+                    stack.push(cur);
             }
         }
+        
+        String res = "";
+        while(!stack.isEmpty()){
+            res = "/" + stack.pop() + res;
+        }
+
         if(res == "") res = "/";
         return res;
+
+        // for(int i = 0; i < strs.length; i++){
+        //     stack.push(strs[i]);
+        // }
+
+        // /*
+        // split the strings by '/' and put them onto the stack
+        // from the stack, add into the resulting string based on the 
+        // */
+        // // System.out.println(Arrays.toString(strs));
+
+        // String res = "";
+        // int skips = 0;
+        // while(!stack.isEmpty()){
+        //     String cur = stack.pop();
+        //     switch(cur){
+        //         case "..":
+        //             skips++;
+        //             break;
+        //         case ".":
+        //             break;
+        //         case "":
+        //             break;
+        //         default:
+        //             if(skips == 0){
+        //                 res = "/" + cur + res;
+        //             }else{
+        //                 skips--;
+        //             }
+        //     }
+        // }
+        // if(res == "") res = "/";
+        // return res;
     }
 }
