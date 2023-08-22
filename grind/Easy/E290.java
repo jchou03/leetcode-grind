8a377409faaae0ffd05bd77d0542fa6919f2ae1a
jchou21 <jared.chou7@gmail.com>
2023-08-21 22:30:58 -0400

    implemented double map solution for E290

--- grind/Easy/E290.java
+++ grind/Easy/E290.java
@@ -8,22 +8,25 @@ public class E290 {
             split the string by spaces
             store the existing pattern in a hashmap
         */
-        HashMap<Character, String> map = new HashMap<Character, String>();
+        HashMap<Character, String> charToStr = new HashMap<Character, String>();
+        HashMap<String, Character> strToChar = new HashMap<String, Character>();
         String[] words = s.split(" ");
         int counter = 0;
         for(int i = 0; i < pattern.length(); i++){
+            if(counter >= words.length){
+                return false;
+            }
             char c = pattern.charAt(i);
             String word = words[counter];
             while(word.equals("")){
                 counter++;
                 word = words[counter];
             }
-            if(!map.containsKey(c)){
-                map.put(c, word);
-            }else{
-                if(!word.equals(map.get(c))){
-                    return false;
-                }
+            if(!charToStr.containsKey(c) && !strToChar.containsKey(word)){
+                charToStr.put(c, word);
+                strToChar.put(word, c);
+            }else if(!word.equals(charToStr.get(c)) || !(c == strToChar.get(word))){
+                return false;
             }
             counter++;
         }
