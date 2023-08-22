eb684b6a848aac46746bf4ef8a6cb7679cbd5fd1
jchou21 <jared.chou7@gmail.com>
2023-08-21 22:16:58 -0400

    working on debugging M138

--- grind/Medium/M138.java
+++ grind/Medium/M138.java
@@ -1,6 +1,11 @@
 
 /* copy linked list with random pointers */
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.w3c.dom.Node;
+
 public class M138 {
     public Node copyRandomList(Node head) {
         // use a multiple iteration approach
@@ -16,14 +21,26 @@ public class M138 {
             counter++;
             trav = trav.next;
         }
+        System.out.println("Map: " + map);
 
+        Node originalTrav = head;
         for(int i = 0; i < list.size() - 1; i++){
             Node cur = list.get(i);
+            System.out.println(cur.val);
             cur.next = list.get(i + 1);
-            cur.random = list.get(map.get(cur.val));
+            
+            Integer random = map.get(originalTrav.random.val);
+            if(random == null){
+                cur.random = null;
+            }else{
+                cur.random = list.get(map.get(originalTrav.random.val));
+            }
+            originalTrav = originalTrav.next;
+            System.out.println("orignalTrav: " + originalTrav.val);
         }
-
-        list.get(list.size() - 1).random = list.get(map.get(list.get(list.size() - 1)));
+        System.out.println("trav random: " + originalTrav.random.val);
+        Node last = list.get(list.size() - 1);
+        last.random = list.get(map.get(originalTrav.random.val));
         return list.get(0);
     }
 }
