59fe9c512befc76698f47c9c84307d35dc52dfa4
jchou21 <jared.chou7@gmail.com>
2023-08-23 21:54:52 -0400

    solved deep random copy of list

--- grind/Medium/M138.java
+++ grind/Medium/M138.java
@@ -8,39 +8,23 @@ import org.w3c.dom.Node;
 
 public class M138 {
     public Node copyRandomList(Node head) {
-        // use a multiple iteration approach
-        // use a map to store the val that a random node points to
-        // use a list to store the vals for each node in LL
-        ArrayList<Node> list = new ArrayList<Node>();
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        int counter = 0;
+        // use a hashmap to create a copy of each node
         Node trav = head;
+        HashMap<Node, Node> map = new HashMap<Node, Node>();
         while(trav != null){
-            list.add(new Node(trav.val));
-            map.put(trav.val, counter);
-            counter++;
+            if(!map.containsKey(trav)){
+                map.put(trav, new Node(trav.val));
+            }
             trav = trav.next;
         }
-        System.out.println("Map: " + map);
-
-        Node originalTrav = head;
-        for(int i = 0; i < list.size() - 1; i++){
-            Node cur = list.get(i);
-            System.out.println(cur.val);
-            cur.next = list.get(i + 1);
-            
-            Integer random = map.get(originalTrav.random.val);
-            if(random == null){
-                cur.random = null;
-            }else{
-                cur.random = list.get(map.get(originalTrav.random.val));
-            }
-            originalTrav = originalTrav.next;
-            System.out.println("orignalTrav: " + originalTrav.val);
+        trav = head;
+        Node newHead = map.get(head);
+        while(trav != null){
+            map.get(trav).next = map.get(trav.next);
+            map.get(trav).random = map.get(trav.random);
+            trav = trav.next;
         }
-        System.out.println("trav random: " + originalTrav.random.val);
-        Node last = list.get(list.size() - 1);
-        last.random = list.get(map.get(originalTrav.random.val));
-        return list.get(0);
+
+        return map.get(head);
     }
 }
