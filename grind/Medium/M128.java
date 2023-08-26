963013af164ebc0066f8c89dd96c4bb359bfa412
jchou21 <jared.chou7@gmail.com>
2023-08-26 19:19:25 -0400

    removed sorting from M128, but worst case could be O(n^2) with updating set references

--- grind/Medium/M128.java
+++ grind/Medium/M128.java
@@ -1,7 +1,6 @@
 package grind.Medium;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
 /*
  * given an input of numbers, return the maximum length of numbers in a sequence
@@ -20,20 +19,44 @@ public class M128 {
                 - if no, add nums[i] into the map with the length of sequence being 1
                 - keep track of the maximum sequence length 
             - this solution assumes that the input array is sorted, but sorting the array isn't in O(n)
+        alternate plan:
+            - create a hashmap that stores the number that it's looking for with a set that stores all of the numbers within it
+                - since set is same underlying object, new additions are added to the same set
+                - then check for set size and get max
         */
-        Arrays.sort(nums);
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        HashMap<Integer, Set<Integer>> map = new HashMap<Integer, Set<Integer>>();
         int maxSeq = 0;
         for(int i = 0; i < nums.length; i++){
-            if(map.containsKey(nums[i] - 1)){
-                int seqLength = map.get(nums[i] - 1) + 1;
-                maxSeq = Math.max(maxSeq, seqLength);
-                map.put(nums[i], seqLength);
-            }else{
-                map.put(nums[i], 1);
-                maxSeq = Math.max(maxSeq, 1);
+            if(!map.containsKey(nums[i] - 1) && !map.containsKey(nums[i] + 1)){
+                Set<Integer> set = new HashSet<Integer>();
+                set.add(nums[i]);
+                map.put(nums[i], set);
             }
-            // System.out.println(map);
+            if(map.containsKey(nums[i] - 1)){
+                Set<Integer> set = map.get(nums[i] - 1);
+                set.add(nums[i]);
+                map.put(nums[i], set);
+            }
+            if(map.containsKey(nums[i] + 1)){
+                if(map.containsKey(nums[i])){
+                    // need to update underlying references for the greater values
+                    Set<Integer> set = map.get(nums[i]);
+                    set.addAll(map.get(nums[i] + 1));
+                    int j = i + 1;
+                    while(map.containsKey(j)){
+                        map.put(j, set);
+                        j++;
+                    }
+                }else{
+                    Set<Integer> set = map.get(nums[i] + 1);
+                    set.add(nums[i]);
+                    map.put(nums[i], set);
+                }
+            }
+        }
+
+        for(Map.Entry<Integer, Set<Integer>> entry : map.entrySet()){
+            maxSeq = Math.max(entry.getValue().size(), maxSeq);
         }
         return maxSeq;
     }
