a731ca8b930c2c39c79e43913e50787798cad468
jchou21 <jared.chou7@gmail.com>
2023-11-28 00:19:24 -0500

    solved M6

--- grind/Medium/M6.java
+++ grind/Medium/M6.java
@@ -1,5 +1,7 @@
 package grind.Medium;
 
+// runtime: 34%, memory: 17%
+
 public class M6 {
     class Solution {
         public String convert(String s, int numRows) {
@@ -7,15 +9,20 @@ public class M6 {
             use an array of strings to store the string of each row then return all of them concated together
             */
     
+            if(numRows <= 1){
+                return s;
+            }
+    
             String[] rows = new String[numRows];
             for(int i = 0; i < numRows; i++){
                 rows[i] = "";
             }
             // now iterate over the entire string and add each character to the appropriate row
+            int cycle = (2 * (numRows - 1));
             for(int i = 0; i < s.length(); i++){
-                int rowIndex = i % (2 * numRows - 1);
+                int rowIndex = i % cycle;
                 if(rowIndex >= numRows){
-                    rowIndex = (2*numRows - 1) - rowIndex;
+                    rowIndex = cycle - rowIndex;
                 }
     
                 rows[rowIndex] += s.charAt(i);
@@ -25,7 +32,7 @@ public class M6 {
     
             for(int i = 0; i < rows.length; i++){
                 res += rows[i];
-                System.out.println(rows[i]);
+                // System.out.println(rows[i]);
             }
     
             return res;
