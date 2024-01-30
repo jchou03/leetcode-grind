1ce7a51fe06fad6619cf9a833e583cd7194f32d9
jchou21 <jared.chou7@gmail.com>
2024-01-30 16:56:50 -0500

    utilizing array hashing

--- grind/Easy/E242.java
+++ grind/Easy/E242.java
@@ -64,5 +64,29 @@ public class E242 {
     }
      * 
      */
+
+     /*
+      * public boolean isAnagram(String s, String t) {
+        
+        use an array to store the characters, and make sure that all letters are matched up
+        
+        if(s.length() != t.length()) return false;
+
+        int[] chars = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            chars[c - 'a']++;
+        }
+        for(int i = 0; i < t.length(); i++){
+            char c = t.charAt(i);
+            chars[c - 'a']--;
+        }
+
+        for(int i = 0; i < chars.length; i++){
+            if(chars[i] != 0) return false;
+        }
+        return true;
+    }
+      */
 }
 
