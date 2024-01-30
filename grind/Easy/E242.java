3f9870286351e3f970d3d3ddcd87e995c31211fa
jchou21 <jared.chou7@gmail.com>
2024-01-30 16:35:37 -0500

    updated solution to E242

--- grind/Easy/E242.java
+++ grind/Easy/E242.java
@@ -1,4 +1,5 @@
 package grind.Easy;
+import java.util.*;
 
 public class E242 {
     public boolean isAnagram(String s, String t) {
@@ -29,5 +30,39 @@ public class E242 {
 
         return cMap.isEmpty();
     }
+
+    /*
+     * public boolean isAnagram(String s, String t) {
+        
+        //use 2 hashmaps to store all the characters (and count of each)
+        
+        HashMap<Character, Integer> m1 = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> m2 = new HashMap<Character, Integer>();
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(m1.containsKey(c)){
+                m1.put(c, m1.get(c) + 1);
+            }else{
+                m1.put(c, 1);
+            }
+        }
+
+        for(int i = 0; i < t.length(); i++){
+            char c = t.charAt(i);
+            if(m2.containsKey(c)){
+                m2.put(c, m2.get(c) + 1);
+            }else{
+                m2.put(c, 1);
+            }
+        }
+
+        if(m1.equals(m2)){
+            return true;
+        }
+        return false;
+    }
+     * 
+     */
 }
 
