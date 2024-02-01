ef5f2734b099e511b197c955e6f5314b3c0a77ec
jchou21 <jared.chou7@gmail.com>
2024-01-31 22:16:22 -0500

    M238 resolved using DP arrays

--- grind/Medium/M238.java
+++ grind/Medium/M238.java
@@ -1,21 +1,51 @@
 package grind.Medium;
 
+/*
+ * product of all elements in array other than self (O(n) with no use of division)
+ * runtime: 100%, memory: 55%
+ */
+
 public class M238 {
     public int[] productExceptSelf(int[] nums) {
-        // use dp to store the left and right products of each index
+        // create two arrays, one that stores the products of all numbers from 0 to i - 1
+        // the other stores the products of all numbers from nums.length - i + 1
+
+        int[] prefix = new int[nums.length];
+        int[] suffix = new int[nums.length];
+
+        prefix[0] = 1;
+        suffix[nums.length - 1] = 1;
+
+        for(int i = 1; i < nums.length; i++){
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+        for(int j = nums.length - 2; j >= 0; j--){
+            suffix[j] = suffix[j + 1] * nums[j + 1];
+        }
+
+        // find product
         int[] res = new int[nums.length];
-
-        int left = 1;
         for(int i = 0; i < nums.length; i++){
-            res[i] = left;
-            left *= nums[i];
+            res[i] = prefix[i] * suffix[i];
         }
-        int right = 1;
-        for(int i = nums.length - 1; i >= 0; i--){
-            res[i] = res[i] * right;
-            right *= nums[i];
-        }
-
         return res;
     }
+
+    // public int[] productExceptSelf(int[] nums) {
+    //     // use dp to store the left and right products of each index
+    //     int[] res = new int[nums.length];
+
+    //     int left = 1;
+    //     for(int i = 0; i < nums.length; i++){
+    //         res[i] = left;
+    //         left *= nums[i];
+    //     }
+    //     int right = 1;
+    //     for(int i = nums.length - 1; i >= 0; i--){
+    //         res[i] = res[i] * right;
+    //         right *= nums[i];
+    //     }
+
+    //     return res;
+    // }
 }
