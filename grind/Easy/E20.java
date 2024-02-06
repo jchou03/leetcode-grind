a881e416a34021a48022031180652cb5071df090
jchou21 <jared.chou7@gmail.com>
2024-02-06 14:11:29 -0500

    resolved E20

--- grind/Easy/E20.java
+++ grind/Easy/E20.java
@@ -2,27 +2,47 @@ package grind.Easy;
 import java.util.*;
 
 // matching parens
+// runtime 64%, memory 56%
 
 public class E20 {
     public boolean isValid(String s) {
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
         Stack<Character> stack = new Stack<Character>();
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
-            if(c == '{' || c == '(' || c == '['){
+            if(map.containsKey(c)){
                 stack.push(c);
-            }else if(c == '}' || c == ')' || c == ']'){
-               if(stack.empty() || !checkParens(stack.pop(), c)) return false;
+            }else{
+                if(stack.isEmpty() || map.get(stack.pop()) != c){
+                    return false;
+                }
             }
         }
-        if(stack.empty()) {
-            return true;
-        }else{
-            return false;            
-        }
 
+        return stack.isEmpty();
     }
-    private boolean checkParens(char a, char b){
-        if((a == '{' && b == '}') || (a == '(' && b == ')') || (a == '[' && b == ']')) return true;
-        return false;
-    }
+    // public boolean isValid(String s) {
+    //     Stack<Character> stack = new Stack<Character>();
+    //     for(int i = 0; i < s.length(); i++){
+    //         char c = s.charAt(i);
+    //         if(c == '{' || c == '(' || c == '['){
+    //             stack.push(c);
+    //         }else if(c == '}' || c == ')' || c == ']'){
+    //            if(stack.empty() || !checkParens(stack.pop(), c)) return false;
+    //         }
+    //     }
+    //     if(stack.empty()) {
+    //         return true;
+    //     }else{
+    //         return false;            
+    //     }
+
+    // }
+    // private boolean checkParens(char a, char b){
+    //     if((a == '{' && b == '}') || (a == '(' && b == ')') || (a == '[' && b == ']')) return true;
+    //     return false;
+    // }
 }
