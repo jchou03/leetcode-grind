be5c6cfcc7748665069596e44185cd918000051c
jchou21 <jared.chou7@gmail.com>
2024-02-06 20:47:47 -0500

    resolved E206

--- grind/Easy/E206.java
+++ grind/Easy/E206.java
@@ -1,5 +1,7 @@
 package grind.Easy;
 
+import java.util.Stack;
+
 // Reverse Linked List
 
 public class E206 {
@@ -12,17 +14,40 @@ public class E206 {
     }
     
     public ListNode reverseList(ListNode head) {
-        return helper(head, null);
+        Stack<ListNode> stack = new Stack<ListNode>();
 
-    }
-    private ListNode helper(ListNode head, ListNode rev){
-        // take current head, and attach it to the front of the rev list so far
-        if(head == null){
-            return rev;
-        }else{
-            ListNode tmp = head.next;
-            head.next = rev;
-            return helper(tmp, head);
+        // store stack of reversed elements
+        while(head != null){
+            stack.add(head);
+            head = head.next;
         }
+
+        if(stack.isEmpty()) return null;
+
+        ListNode returnHead = stack.pop();
+        ListNode trav = returnHead;
+        while(!stack.isEmpty()){
+            trav.next = stack.pop();
+            trav = trav.next;
+        }
+
+        trav.next = null;
+
+        return returnHead;
     }
+
+    // public ListNode reverseList(ListNode head) {
+    //     return helper(head, null);
+
+    // }
+    // private ListNode helper(ListNode head, ListNode rev){
+    //     // take current head, and attach it to the front of the rev list so far
+    //     if(head == null){
+    //         return rev;
+    //     }else{
+    //         ListNode tmp = head.next;
+    //         head.next = rev;
+    //         return helper(tmp, head);
+    //     }
+    // }
 }
