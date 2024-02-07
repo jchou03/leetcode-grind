f63f6c8e004a2beeeb8c65c7dbe27dbc6f9f797a
jchou21 <jared.chou7@gmail.com>
2024-02-06 21:53:39 -0500

    resolved E21 without recursion

--- grind/Easy/E21.java
+++ grind/Easy/E21.java
@@ -36,4 +36,50 @@ public class E21 {
             helper(l1, l2.next, tail.next);
         }
     }
+
+    /*
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // use a trav for each of the lists and greedily add the smaller of the 2 since both lists are sorted
+        ListNode head = (list1 != null && list2 == null) || (list1 != null && list2 != null && list1.val <= list2.val) ? list1 : list2;
+        ListNode trav = head;
+        if(head == null) return head;
+
+        if(list1 != null && head.val == list1.val){
+            list1 = list1.next;
+        }else{
+            list2 = list2.next;
+        }
+
+        while(list1 != null || list2 != null){
+            // ListNode travler = list2;
+            
+            // System.out.printf("l1: %d, l2: %d, trav: %d\n", list1.val, list2.val, trav.val);
+            
+            // while(travler != null){
+            //     System.out.printf("%d,", travler.val);
+            //     travler = travler.next;
+            // }
+
+            // System.out.println();
+
+            if((list1 != null && list2 == null) || (list1 != null && list2 != null && list1.val < list2.val)){
+                // System.out.println("case 1");
+                trav.next = list1;
+                list1 = list1.next;
+                trav = trav.next;
+            }else{
+                // System.out.println("case 2");
+                trav.next = list2;
+                // System.out.println(list2.val);
+                // System.out.println(list2.next.val);
+                list2 = list2.next;
+                trav = trav.next;
+
+                // System.out.println(list2.val);
+            }
+        }
+
+        return head;
+    }
+     */
 }
