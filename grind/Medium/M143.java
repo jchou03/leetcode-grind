959b3aa21d5db0221b89dafe946a842b8bd5bfb4
jchou21 <jared.chou7@gmail.com>
2024-02-08 02:53:28 -0500

    M143 solve

---
+++ grind/Medium/M143.java
@@ -0,0 +1,32 @@
+package grind.Medium;
+
+import java.util.ArrayList;
+
+public class M143 {
+    public void reorderList(ListNode head) {
+        ArrayList<ListNode> arr = new ArrayList<ListNode>();
+
+        ListNode trav = head;
+        while(trav != null){
+            arr.add(trav);
+            trav = trav.next;
+        }
+
+        trav = head;
+        int i = 0;
+        while(i < arr.size()/2){
+            trav.next = arr.get(i);
+            trav = trav.next;
+            trav.next = arr.get(arr.size() - 1 - i);
+            trav = trav.next;
+            i++;
+        }
+
+        if(arr.size() % 2 == 1){
+            trav.next = arr.get((arr.size()/2));
+            trav = trav.next;
+        }
+
+        trav.next = null;
+    }
+}
