2a7386f9119ec08a45263319cd4be0cff08996bd
jchou21 <jared.chou7@gmail.com>
2024-02-08 23:05:49 -0500

    solved E141 without sets

---
+++ grind/Easy/E141.java
@@ -0,0 +1,25 @@
+package grind.Easy;
+
+/*
+Check if LinkedList has a cycle
+using fast & slow travlers
+
+runtime: 100%, memory: 30%
+ */
+
+public class E141 {
+    public boolean hasCycle(ListNode head) {
+        if(head == null) return false;
+        // use a fast and slow racer
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) return true;
+        }
+
+        return false;
+    }
+}
